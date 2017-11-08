e2fba094fa9ebdac429e384212214b69c9f10aef
comp500 <comp500@users.noreply.github.com>
2017-11-08 16:44:23 +0000

    Major rewrite and simplification

--- index.js
+++ index.js
@@ -1,52 +1,38 @@
 /*eslint-disable no-console*/
 
 const tls = require("tls");
-const net = require('net');
+const net = require("net");
 
 const options = {
-  host: "",
-  servername: "",
-  port: 443
+	host: process.argv[2],
+	servername: process.argv[2],
+	port: 443,
+	rejectUnauthorized: false
 };
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const server = net.createServer((tcpSocket) => {
+	console.log("Connection received from " + tcpSocket.remoteAddress);
 
-var socket = tls.connect(options, () => {
-	console.log('client (tls) connected', socket.authorized ? 'authorized' : 'unauthorized');
-	server.listen(3380, () => {
-		console.log('server bound');
+	var tlsSocket = tls.connect(options, () => {
+		console.log("Connected to TLS server:", tlsSocket.authorized ? "authorized" : "unauthorized");
+		tlsSocket.fd = tcpSocket.fd;
+		tcpSocket.pipe(tlsSocket);
+		tlsSocket.pipe(tcpSocket);
 	});
-});
 
-socket.on('error', (err) => {
-	console.dir(err);
-});
-//socket.setEncoding('utf8');
-
-const server = net.createServer((c) => {
-	console.log("Connection from " + c.remoteAddress);
-	console.log("Connection to " + socket.remoteAddress);
-	socket.fd = c.fd;
-	c.pipe(socket);
-	socket.pipe(c);
-	// 'connection' listener
+	tlsSocket.on("error", (err) => {
+		console.dir(err);
+	});
 	
-	c.on('end', () => {
-		console.log('client disconnected');
-		socket.end();
-		socket = tls.connect(options, () => {
-			console.log('client (tls) connected', socket.authorized ? 'authorized' : 'unauthorized');
-			server.listen(3380, () => {
-				console.log('server bound');
-			});
-		});
-		
-		socket.on('error', (err) => {
-			console.dir(err);
-		});
-		//server.close();
+	tcpSocket.on("end", () => {
+		console.log("Client disconnected");
 	});
 });
-server.on('error', (err) => {
+
+server.on("error", (err) => {
 	console.dir(err);
+});
+
+server.listen(3380, () => {
+	console.log("TCP server started");
 });
