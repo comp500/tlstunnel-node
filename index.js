b2d094abda5ab299b63b8e9adc57e40c11d3e68a
comp500 <comp500@users.noreply.github.com>
2017-11-08 17:15:52 +0000

    Fingerprint list, and checking for fingerprints. Also drop socket on error.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const tls = require("tls");
 const net = require("net");
 
+const fingerprintList = [ "38:8C:40:F2:B9:75:E1:E6:DA:57:37:57:C8:21:92:EF:B5:0D:73:93" ];
+
 const options = {
 	host: process.argv[2],
 	servername: process.argv[2],
@@ -20,26 +22,47 @@ const server = net.createServer((tcpSocket) => {
 	var tlsSocket = tls.connect(options, () => {
 		console.log("[" + socketId + "]", "Connected to TLS server:", tlsSocket.authorized ? "authorized" : "unauthorized");
 
-		if (!tlsSocket.authorized) {
-			console.log("[" + socketId + "]", "Authorization error:", tlsSocket.authorizationError);
-			console.log("[" + socketId + "]", "Certificate details:", JSON.stringify(tlsSocket.getPeerCertificate().subject));
-		}
+		var cert = tlsSocket.getPeerCertificate();
 
-		// Sync file descriptors, for some reason
-		tlsSocket.fd = tcpSocket.fd;
-		// Pipe TCP -> TLS
-		tcpSocket.pipe(tlsSocket);
-		// Pipe TLS -> TCP
-		tlsSocket.pipe(tcpSocket);
+		if (fingerprintList.indexOf(cert.fingerprint.toUpperCase()) == -1) {
+			if (tlsSocket.authorized) {
+				console.log("[" + socketId + "]", "Certificate not in fingerprint list");
+			} else {
+				console.log("[" + socketId + "]", "Authorization error:", tlsSocket.authorizationError);
+			}
+
+			console.log("[" + socketId + "]", "Certificate details:", JSON.stringify(cert.subject));
+			console.log("[" + socketId + "]", "Certificate fingerprint:", cert.fingerprint);
+			console.log("[" + socketId + "]", "If you trust the above certificate, copy it to the fingerprint list in config.json");
+
+			tlsSocket.end();
+			tcpSocket.end();
+		} else {
+			console.log("[" + socketId + "]", "Fingerprint verified:", cert.fingerprint);
+
+			// Sync file descriptors, for some reason
+			tlsSocket.fd = tcpSocket.fd;
+			// Pipe TCP -> TLS
+			tcpSocket.pipe(tlsSocket);
+			// Pipe TLS -> TCP
+			tlsSocket.pipe(tcpSocket);
+		}
 	});
 
 	tlsSocket.on("error", (err) => {
 		console.log("Socket error, id", socketId);
 		console.dir(err);
+		tcpSocket.end();
+	});
+
+	tcpSocket.on("error", (err) => {
+		console.log("Socket error, id", socketId);
+		console.dir(err);
+		tlsSocket.end();
 	});
 	
 	tcpSocket.on("end", () => {
-		console.log("[" + socketId + "]", "Client / Server disconnected");
+		console.log("[" + socketId + "]", "Socket disconnected");
 	});
 });
 
